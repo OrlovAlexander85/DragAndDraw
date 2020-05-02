46c7929c107d3a1f0c584a29a7f7750d09fa3fb1
OrlovAlexander <6816106@gmail.com>
2020-05-02 09:33:24 +0300

    Initial commit

---
+++ app/src/main/java/ru/orlovph/draganddraw/BoxDrawingView.java
@@ -0,0 +1,61 @@
+package ru.orlovph.draganddraw;
+
+import android.content.Context;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoxDrawingView extends View {
+    private static final String TAG = "BoxDrawingView";
+
+    private Box currentBox;
+    private List<Box> boxen = new ArrayList<>();
+    // Used when creating the view in code
+    public BoxDrawingView(Context context){
+        this(context, null);
+    }
+
+    // Used when inflating the view from XML
+    public BoxDrawingView(Context context, AttributeSet attrs) {
+        super (context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(),event.getY());
+        String action = "";
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                // Reset drawing state
+                currentBox = new Box(current);
+                boxen.add(currentBox);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                if (currentBox != null){
+                    currentBox.setCurrent(current);
+                    invalidate();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                currentBox = null;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                currentBox = null;
+                break;
+        }
+
+        Log.i(TAG, action + " at x=" + current.x +
+                ", y=" + current.y);
+        return true;
+    }
+}
